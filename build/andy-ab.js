10a42f310216223ce3c65868d122269829946d27
Andy Stabler <andy_60@hotmail.co.uk>
2016-07-23 22:02:44 +0100

    Run grunt

--- build/andy-ab.js
+++ build/andy-ab.js
@@ -1,98 +1,145 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.AndyAB = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/*jshint: strict:true*/
+var AndyAB =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
 
-var AndyAB = function(name) {
-  this.name = name || "";
-  this.cohorts = [];
-  this.listeners = [];
-  this.MutationObserver = window.MutationObserver ||
-    window.WebKitMutationObserver ||
-    window.MozMutationObserver;
-  this.observer = new MutationObserver(this.check.bind(this));
-  this.observer.observe(window.document, {
-    childList: true,
-    subtree: true
-  });
-  this.endTime = new Date();
-  this.endTime.setDate(this.endTime.getDate() + 1);
-  this.endTime = this.endTime.getTime();
-  this.cookiePrefix = "AB";
-};
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
 
-AndyAB.prototype.withCohorts = function(cohorts) {
-  this.cohorts = cohorts;
-  return this;
-};
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
 
-AndyAB.prototype.endingAtTime = function(date) {
-  this.endTime = date;
-  return this;
-};
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
 
-AndyAB.prototype.sampleCohort = function() {
-  if (this.cohorts.length === 0)
-    return;
-  var max = this.cohorts.length - 1,
-  min = 0,
-  cohort = Math.floor(Math.random()*(max-min+1)+min);
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 
-  return this.cohorts[cohort];
-};
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
 
-AndyAB.prototype.enrol = function(callback) {
-  var cohort = this.getCookie(this.cookiePrefix + this.name);
-  if (cohort === null) {
-    cohort = this.sampleCohort();
-    this.setCookie(this.cookiePrefix + this.name, cohort);
-    if (typeof callback === 'function')
-      callback(cohort);
-  }
-  return this;
-};
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
 
-AndyAB.prototype.getCohort = function() {
-  return this.getCookie(this.cookiePrefix + this.name);
-};
 
-AndyAB.prototype.setCookie = function(key, value) {
-  document.cookie = key + '=' + value + ';expires=' + this.endTime;
-};
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
 
-AndyAB.prototype.getCookie = function(key) {
-  var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
-  return keyValue ? keyValue[2] : null;
-};
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
 
-AndyAB.prototype.ready = function(selector, callback) {
-  this.listeners.push({
-    selector: selector,
-    callback: callback
-  });
-  this.check();
-};
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
 
-AndyAB.prototype.updateForCohort = function(cohort, selector, callback) {
-  if (this.getCohort() == cohort)
-    this.ready(selector, callback);
-};
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports) {
 
-AndyAB.prototype.check = function() {
-  var listenersLength = this.listeners.length, listener, elements;
-  for (var i = 0; i < listenersLength; i++) {
-    listener = this.listeners[i];
-    elements = window.document.querySelectorAll(listener.selector);
-    var elementsLength = elements.length, element;
-    for (var j = 0; j < elementsLength; j++) {
-      element = elements[j];
-      if (!element.ready) {
-        element.ready = true;
-        listener.callback.call(element, element);
-      }
-    }
-  }
-};
+	/*jshint: strict:true*/
 
-module.exports = AndyAB;
+	var AndyAB = function(name) {
+	  this.name = name || "";
+	  this.cohorts = [];
+	  this.listeners = [];
+	  this.MutationObserver = window.MutationObserver ||
+	    window.WebKitMutationObserver ||
+	    window.MozMutationObserver;
+	  this.observer = new MutationObserver(this.check.bind(this));
+	  this.observer.observe(window.document, {
+	    childList: true,
+	    subtree: true
+	  });
+	  this.endTime = new Date();
+	  this.endTime.setDate(this.endTime.getDate() + 1);
+	  this.endTime = this.endTime.getTime();
+	  this.cookiePrefix = "AB";
+	};
 
-},{}]},{},[1])(1)
-});
+	AndyAB.prototype.withCohorts = function(cohorts) {
+	  this.cohorts = cohorts;
+	  return this;
+	};
+
+	AndyAB.prototype.endingAtTime = function(date) {
+	  this.endTime = date;
+	  return this;
+	};
+
+	AndyAB.prototype.sampleCohort = function() {
+	  if (this.cohorts.length === 0)
+	    return;
+	  var max = this.cohorts.length - 1,
+	  min = 0,
+	  cohort = Math.floor(Math.random()*(max-min+1)+min);
+
+	  return this.cohorts[cohort];
+	};
+
+	AndyAB.prototype.enrol = function(callback) {
+	  var cohort = this.getCookie(this.cookiePrefix + this.name);
+	  if (cohort === null) {
+	    cohort = this.sampleCohort();
+	    this.setCookie(this.cookiePrefix + this.name, cohort);
+	    if (typeof callback === 'function')
+	      callback(cohort);
+	  }
+	  return this;
+	};
+
+	AndyAB.prototype.getCohort = function() {
+	  return this.getCookie(this.cookiePrefix + this.name);
+	};
+
+	AndyAB.prototype.setCookie = function(key, value) {
+	  document.cookie = key + '=' + value + ';expires=' + this.endTime;
+	};
+
+	AndyAB.prototype.getCookie = function(key) {
+	  var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+	  return keyValue ? keyValue[2] : null;
+	};
+
+	AndyAB.prototype.ready = function(selector, callback) {
+	  this.listeners.push({
+	    selector: selector,
+	    callback: callback
+	  });
+	  this.check();
+	};
+
+	AndyAB.prototype.updateForCohort = function(cohort, selector, callback) {
+	  if (this.getCohort() == cohort)
+	    this.ready(selector, callback);
+	};
+
+	AndyAB.prototype.check = function() {
+	  var listenersLength = this.listeners.length, listener, elements;
+	  for (var i = 0; i < listenersLength; i++) {
+	    listener = this.listeners[i];
+	    elements = window.document.querySelectorAll(listener.selector);
+	    var elementsLength = elements.length, element;
+	    for (var j = 0; j < elementsLength; j++) {
+	      element = elements[j];
+	      if (!element.ready) {
+	        element.ready = true;
+	        listener.callback.call(element, element);
+	      }
+	    }
+	  }
+	};
+
+	module.exports = AndyAB;
+
+
+/***/ }
+/******/ ]);
