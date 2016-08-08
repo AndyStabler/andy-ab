11adfeca20c3bcc20e4bb4cbeed8ecf5aa435e48
Andy Stabler <andy_60@hotmail.co.uk>
2016-08-08 23:51:22 +0100

    Replace Jasmine tests with Mocha

--- test/andyAbSpec.js
+++ test/andyAbSpec.js
@@ -1,5 +1,13 @@
+var jsdom = require('jsdom');
+
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var expect = chai.expect;
+chai.use(sinonChai);
+
 var AndyAB = require("../lib/andy-ab.js");
-var Observer = require("./../lib/observer.js");
+var Observer = require("../lib/observer.js");
 
 describe("AndyAB", function() {
   var ab;
@@ -15,14 +23,14 @@ describe("AndyAB", function() {
 
   describe("sampleCohort", function() {
     it("should raise an error when the cohorts aren't set", function() {
-      expect(ab.sampleCohort).toThrow();
+      expect(ab.sampleCohort).to.throw();
     });
 
     it("should select a cohort", function() {
       ab.withCohorts(["treatment", "control"]);
       enrolledCohort = ab.sampleCohort();
-      expect(enrolledCohort).toBeDefined();
-      expect(ab.cohorts).toContain(enrolledCohort);
+      expect(enrolledCohort).to.not.be.undefined;
+      expect(ab.cohorts).to.contain(enrolledCohort);
     });
   });
 
@@ -39,45 +47,49 @@ describe("AndyAB", function() {
       });
 
       it("should not reset the cookie", function() {
-        var setCookie = spyOn(ab, "setCookie");
+        var setCookie = sinon.spy(ab, "setCookie")
         ab.enrol();
-        expect(setCookie).not.toHaveBeenCalled();
+        expect(setCookie).not.to.have.been.called;
       });
 
       it("should not execute the enrol callback", function() {
-        var callback = jasmine.createSpy("enrolled!");
+        var callback = sinon.spy();
         ab.enrol(callback);
-        expect(callback).not.toHaveBeenCalled();
+        expect(callback).not.to.have.been.called;
       });
     });
 
     describe("when not already enrolled ", function(){
       it("should enrol the user into a cohort", function() {
-        expect(ab.getCohort()).toEqual("");
+        expect(ab.getCohort()).to.be.empty;
         ab.enrol();
-        expect(ab.getCohort()).toBeTruthy();
+        expect(ab.getCohort()).to.be.ok;
       });
 
       it("should execute the enrol callback", function() {
-        var callback = jasmine.createSpy("enrolled!");
+        var callback = sinon.spy();
         ab.enrol(callback);
-        expect(callback).toHaveBeenCalled();
+        expect(callback).to.have.been.called;
       });
     });
+  });
+
+  describe("getCohort", function() {
+
   });
 
   describe("cookieName", function() {
     describe("when the cookie prefix is set", function() {
       it("should include the prefix", function() {
         ab.cookiePrefix = "prefix";
-        expect(ab.cookieName()).toEqual(ab.cookiePrefix + ab.name);
+        expect(ab.cookieName()).to.equal(ab.cookiePrefix + ab.name);
       });
     });
 
     describe("when the cookie prefix is not set", function() {
       it("should not include a prefix", function() {
         ab.cookiePrefix = "";
-        expect(ab.cookieName()).toEqual(ab.name);
+        expect(ab.cookieName()).to.equal(ab.name);
       });
     });
   });
@@ -89,10 +101,10 @@ describe("AndyAB", function() {
 
     describe("when the cohort matches the enrolled cohort", function() {
       it("should add the observer to the observers array", function() {
-        var notifyAllObservers = spyOn(ab, "notifyAllObservers");
+        var notifyAllObservers = sinon.spy(ab, "notifyAllObservers");
         ab.addObserver(ab.getCohort(), "#cake", function() {});
-        expect(notifyAllObservers).toHaveBeenCalled();
-        expect(ab.observers.length).toBe(1);
+        expect(notifyAllObservers).to.have.been.called;
+        expect(ab.observers.length).to.equal(1);
       });
     });
 
@@ -103,30 +115,23 @@ describe("AndyAB", function() {
         var otherCohort = ab.cohorts.filter(function(el) {
           return el !== cohort;
         })[0];
-        var notifyAllObservers = spyOn(ab, "notifyAllObservers");
+        var notifyAllObservers = sinon.spy(ab, "notifyAllObservers");
         ab.addObserver(otherCohort, "#cake", function() {});
-        expect(notifyAllObservers).not.toHaveBeenCalled();
-        expect(ab.observers.length).toBe(0);
+        expect(notifyAllObservers).not.to.have.been.called;
+        expect(ab.observers.length).to.equal(0);
       });
     });
   });
 
   describe("notifyAllObservers", function() {
     it("should notify each observer", function() {
-      observers = [
-        new Observer("#cake", function() {}),
-        new Observer("#cake", function() {}),
+      var observers = [
         new Observer("#cake", function() {})
       ];
       ab.observers = observers;
-      var spies = [];
-      observers.forEach(function(el, index) {
-        spies[index] = spyOn(el, "notify");
-      });
+      var notify = sinon.spy(observers[0], "notify");
       ab.notifyAllObservers();
-      spies.forEach(function(el) {
-        expect(el.notify).toHaveBeenCalled();
-      });
+      expect(notify).to.have.been.called;
     });
   });
 
