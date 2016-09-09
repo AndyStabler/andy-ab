5e3910f225f3e7b6afbd74a4ecbd322c41cce5ac
Andy Stabler <andy_60@hotmail.co.uk>
2016-09-09 19:14:04 +0100

    Add an ExperimentCookie class

---
+++ test/experimentCookieSpec.js
@@ -0,0 +1,68 @@
+var jsdom = require('jsdom');
+
+var chai = require("chai");
+var sinon = require("sinon");
+var sinonChai = require("sinon-chai");
+var expect = chai.expect;
+chai.use(sinonChai);
+
+var ExperimentCookie = require("../lib/experiment-cookie.js");
+var Cookies = require("js-cookie");
+
+describe("ExperimentCookie", function() {
+  var cookie;
+
+  beforeEach(function() {
+    cookie = new ExperimentCookie("AndyAB");
+  });
+
+  afterEach(function() {
+    Cookies.remove(cookie.name);
+  });
+
+  describe("getCohort", function() {
+    it("should return undefined when the cookie does not exist", function() {
+      var cohort = cookie.getCohort("test1");
+      expect(cohort).to.be.undefined;
+    });
+
+    describe("when the cookie exists", function() {
+      beforeEach(function() {
+        Cookies.set(cookie.name, "{}");
+      });
+
+      it("should return undefined when the experiment is not present", function() {
+        expect(cookie.getCohort("test1")).to.be.undefined;
+      });
+
+      it("should return the cohort", function() {
+        cookie.setCohort("test1", "control");
+        expect(cookie.getCohort("test1")).to.equal("control");
+      });
+    });
+  });
+
+  describe("setCohort", function() {
+    it("should create and set the cookie when it does not exist", function() {
+      cookie.setCohort("test1", "control");
+      expect(cookie.getCohort("test1")).to.equal("control");
+    });
+
+    describe("when the cookie exists", function() {
+      beforeEach(function() {
+        Cookies.set(cookie.name, "{}");
+      });
+
+      it("should overwrite the existing cohort when the experiment is present", function() {
+        cookie.setCohort("test1", "control");
+        cookie.setCohort("test1", "treatment");
+        expect(cookie.getCohort("test1")).to.equal("treatment");
+      });
+
+      it("should set the cohort", function() {
+        cookie.setCohort("test1", "treatment");
+        expect(cookie.getCohort("test1")).to.equal("treatment");
+      });
+    });
+  });
+});
